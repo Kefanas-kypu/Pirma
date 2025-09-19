6938eb80706f9a82b54d540477aa3f49ea7cf58b
Kefanas-kypu <kuznecovas.kevinas@gmail.com>
2025-09-19 10:44:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <iomanip> //nes reikia setw, left, right
 #include <vector>
 #include <string>
-//
+#include <algorithm>
+
+
 using namespace std;//komanda privercia kompiliatoriu ziureti i ...
 //nepataria naudoti using namespace std;
 //patogu, bet jei naudosim kitus kintamuosius, tai tada gali susigadinti
@@ -24,6 +26,7 @@ struct studentas{
     vector <int> paz; //kadangi saugosiu masyvus, tai reikia
     int egzaminas;
     double gal_rezultatas;//galima naudoti ir double, pagal nora ir reikala. galutinis pazymys sveikas, todel int
+    double mediana;
     };
  studentas stud_iv();
 
@@ -36,13 +39,14 @@ int main(){
       grupe.push_back(stud_iv());
 //    studentas pastovus;
 //    pastovus = stud_iv();
-    cout<<setw(10)<<left<<"Vardas"<<"|"<<setw(15)<<right<<"Pavarde"<<"|"<<setw(5)<<left<<"Galutinis (Vid.)"<<endl;
-    cout<<"-------------------------------------------"<<endl;
+    cout<<setw(10)<<left<<"Vardas"<<"|"<<setw(15)<<right<<"Pavarde"<<"|"<<setw(20)<<right<<"Galutinis (Vid.)"<<"|"<<setw(20)<<right<<"Galutinis (Med.)"<<endl;
+    cout<<"--------------------------------------------------------------------"<<endl;
     for (auto pastovus:grupe)
     {
         cout<<setw(10)<<left<<pastovus.vardas<<"|"<<setw(15)<<right<<pastovus.pavarde<<"|";
         // for (auto& a : pastovus.paz) cout<<setw(5)<<a<<"|";
-        cout/*<<setw(5)<<pastovus.egzaminas<<"|"*/<<fixed<<setprecision(2)<<setw(5)<<left<<pastovus.gal_rezultatas<<endl;
+        cout/*<<setw(5)<<pastovus.egzaminas<<"|"*/<<fixed<<setprecision(2)<<setw(20)<<right<<pastovus.gal_rezultatas<<"|";
+        cout<<fixed<<setprecision(2)<<setw(20)<<right<<pastovus.mediana<<endl;
     }
     }
     //mes turim viena studenta. mums reikia kazkokio ciklo, kad butu daugiau studentu. sugalvoti kaip suvesti, ka padaryti.
@@ -62,6 +66,15 @@ int main(){
         }
         cout<<"Iveskite egzamino paz: "; cin>>pirmas.egzaminas;
         pirmas.gal_rezultatas= double(sum)/double(n) * 0.4 + pirmas.egzaminas * 0.6; //kazkodel reikia parasyti double
+
+        // medianos radimas
+        vector <int> paz_temp = pirmas.paz; // laikinas pazymiu vektorius, kad butu galima medianos skaiciavimui panaudoti ir egzamino rezultata
+        paz_temp.push_back(pirmas.egzaminas);
+        int kiek = paz_temp.size();
+        sort(paz_temp.begin(),paz_temp.end());
+        if(kiek%2 != 0) pirmas.mediana = (double)paz_temp[kiek / 2];
+            else pirmas.mediana = (double)(paz_temp[(kiek - 1) / 2] + paz_temp[kiek / 2]) / 2.0;
+
         return pirmas;
 
     }
