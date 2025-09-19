67d5d522e7e2de652b5f9d8f0ef91e91a61660be
Kefanas-kypu <kuznecovas.kevinas@gmail.com>
2025-09-19 10:48:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-#include <iomanip> //nes reikia setw, left, right
+#include <iomanip>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
-
-using namespace std;//komanda privercia kompiliatoriu ziureti i ...
-//nepataria naudoti using namespace std;
-//patogu, bet jei naudosim kitus kintamuosius, tai tada gali susigadinti
-//pataria naudoti using std::cout;
-//taip pat su visais pvz cin, endl;
+using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -19,68 +16,153 @@ using std::setw;
 using std::left;
 using std::right;
 
-//struktura aprasoma rasant struct
-struct studentas{
+struct studentas
+{
     string vardas;
     string pavarde;
-    vector <int> paz; //kadangi saugosiu masyvus, tai reikia
+    vector<int> paz;
     int egzaminas;
-    double gal_rezultatas;//galima naudoti ir double, pagal nora ir reikala. galutinis pazymys sveikas, todel int
+    double gal_rezultatas;
     double mediana;
-    };
- studentas stud_iv();
+};
 
-int main(){
+studentas stud_iv();
+void spausdinti_grupe(vector<studentas>& grupe);
+int main()
+{
+    srand(time(0));
+
     vector<studentas> grupe;
-    cout<<"Kiek studentu grupeje? ";
-    int m;
-    cin>>m;
-    for(auto z=0;z<m;z++)
-      grupe.push_back(stud_iv());
-//    studentas pastovus;
-//    pastovus = stud_iv();
-    cout<<setw(10)<<left<<"Vardas"<<"|"<<setw(15)<<right<<"Pavarde"<<"|"<<setw(20)<<right<<"Galutinis (Vid.)"<<"|"<<setw(20)<<right<<"Galutinis (Med.)"<<endl;
-    cout<<"--------------------------------------------------------------------"<<endl;
-    for (auto pastovus:grupe)
+    int pasirinkimas;
+
+    do
     {
-        cout<<setw(10)<<left<<pastovus.vardas<<"|"<<setw(15)<<right<<pastovus.pavarde<<"|";
-        // for (auto& a : pastovus.paz) cout<<setw(5)<<a<<"|";
-        cout/*<<setw(5)<<pastovus.egzaminas<<"|"*/<<fixed<<setprecision(2)<<setw(20)<<right<<pastovus.gal_rezultatas<<"|";
-        cout<<fixed<<setprecision(2)<<setw(20)<<right<<pastovus.mediana<<endl;
-    }
-    }
-    //mes turim viena studenta. mums reikia kazkokio ciklo, kad butu daugiau studentu. sugalvoti kaip suvesti, ka padaryti.
-    studentas stud_iv(){
-        int n=0, laik_paz, sum=0;
-        cout<<"Sveiki"<<endl;
-        studentas pirmas;
-        cout<<"Ivesk studento duomenis"<<endl;
-        cout<<"Vardas: "; cin>>pirmas.vardas;
-        cout<<"Pavarde: "; cin>>pirmas.pavarde;
-        //cout<<"Kiek pazymiu turi " <<pirmas.vardas<<" "<<pirmas.pavarde<<" "; cin>>n;
-        //for (int a=0;a<n;a++)
-        int skaitliukas = 1;
-        while(skaitliukas!=0)
+        cout << "Ka norite atlikti?"<<endl;
+        cout << "1. Ivesti nauja studenta"<<endl;;
+        cout << "2. Atspausdinti rezultatus"<<endl;
+        cout << "3. Iseiti"<<endl;
+        cout << "Pasirinkite veiksma: "<<endl;
+        cin >> pasirinkimas;
+
+        switch (pasirinkimas)
         {
-            cout<<"Iveskite pazymi: "; cin >>laik_paz;
-            pirmas.paz.push_back(laik_paz);
-            sum+=laik_paz;  //alternatyva sum+=pirmas.paz[a]
-            n++;
-            cout<<"Jei daugiau pazymiu studentas neturi, iveskite 0. Kitu atveju iveskite bet koki skaiciu."<<endl;
-            cin>>skaitliukas;
+        case 1:
+            grupe.push_back(stud_iv());
+            break;
+        case 2:
+            spausdinti_grupe(grupe);
+            break;
+        case 3:
+            cout << "Pabaiga."<<endl;
+            break;
+        default:
+            cout << "Neteisingas pasirinkimas."<<endl;
         }
-        cout<<"Paz suma "<<sum<<endl;
-        cout<<"Iveskite egzamino paz: "; cin>>pirmas.egzaminas;
-        pirmas.gal_rezultatas= double(sum)/double(n) * 0.4 + pirmas.egzaminas * 0.6; //kazkodel reikia parasyti double
-
-        // medianos radimas
-        vector <int> paz_temp = pirmas.paz; // laikinas pazymiu vektorius, kad butu galima medianos skaiciavimui panaudoti ir egzamino rezultata
-        paz_temp.push_back(pirmas.egzaminas);
-        int kiek = paz_temp.size();
-        sort(paz_temp.begin(),paz_temp.end());
-        if(kiek%2 != 0) pirmas.mediana = (double)paz_temp[kiek / 2];
-            else pirmas.mediana = (double)(paz_temp[(kiek - 1) / 2] + paz_temp[kiek / 2]) / 2.0;
-
-        return pirmas;
 
     }
+    while (pasirinkimas != 3);
+
+    return 0;
+}
+
+studentas stud_iv()
+{
+    studentas pirmas;
+    int sum = 0, n = 0;
+    int laik_paz;
+
+    cout << "Iveskite studento duomenis"<<endl;
+    cout << "Vardas: "<<endl;
+    cin >> pirmas.vardas;
+    cout << "Pavarde: "<<endl;
+    cin >> pirmas.pavarde;
+
+    int generuoti;
+    cout << "Ar pazymius generuoti atsitiktinai? (1 - generuoti, 0 - vesti ranka): "<<endl;
+    cin >> generuoti;
+
+    if (generuoti == 1)
+    {
+        int kiek = rand() % 6 + 5; // 5-10 pazymiu
+        for (int i = 0; i < kiek; i++)
+        {
+            int random_paz = rand() % 10 + 1; // 1-10
+            pirmas.paz.push_back(random_paz);
+            sum += random_paz;
+            n++;
+        }
+        pirmas.egzaminas = rand() % 10 + 1;
+    }
+    else
+    {
+        int kl;
+        cout<<"Ar zinote, kiek pazymiu turi "<<pirmas.vardas<<" "<<pirmas.pavarde<<"?"<<" Taip - 1, ne - 0."<<endl;
+        cin>>kl;
+
+        if (kl==0)
+        {
+            int laik_paz = -1;
+            while (laik_paz != 0)
+            {
+                cout << "Jei daugiau pazymiu studentas neturi, iveskite 0, kitu atveju iveskite pazymi: ";
+                cin >> laik_paz;
+                pirmas.paz.push_back(laik_paz);
+                sum += laik_paz;
+                n++;
+            }
+            cout << "Iveskite egzamino pazymi: ";
+            cin >> pirmas.egzaminas;
+        }
+        else
+        {
+            cout<<"Kiek pazymiu turi " <<pirmas.vardas<<" "<<pirmas.pavarde<<" ";
+            cin>>n;
+            for (int a=0; a<n; a++)
+            {
+                cout<<a+1<<": ";
+                cin >>laik_paz;
+                pirmas.paz.push_back(laik_paz);
+                sum+=laik_paz;  //alternatyva sum+=pirmas.paz[a]
+            }
+            cout<<"Iveskite egzamino pazymi: ";
+            cin>>pirmas.egzaminas;
+
+        }
+    }
+
+    // Galutinis rezultatas (vidurkis)
+    pirmas.gal_rezultatas = double(sum) / double(n) * 0.4 + pirmas.egzaminas * 0.6;
+
+    // Mediana
+    vector<int> paz_temp = pirmas.paz;
+    paz_temp.push_back(pirmas.egzaminas);
+    sort(paz_temp.begin(), paz_temp.end());
+    int kiek = paz_temp.size();
+    if (kiek % 2 != 0) pirmas.mediana = double(paz_temp[kiek / 2]);
+    else pirmas.mediana = double(paz_temp[(kiek - 1) / 2] + paz_temp[kiek / 2]) / 2.0;
+
+    return pirmas;
+}
+
+void spausdinti_grupe(vector<studentas>& grupe)
+{
+    if (grupe.empty())
+    {
+        cout << "Nera studentu"<<endl;
+        return;
+    }
+
+    cout << setw(10) << left << "Vardas" << "|"
+         << setw(15) << right << "Pavarde" << "|"
+         << setw(20) << right << "Galutinis (Vid.)" << "|"
+         << setw(20) << right << "Galutinis (Med.)" << endl;
+    cout << "--------------------------------------------------------------------"<<endl;
+
+    for (auto& stud : grupe)
+    {
+        cout << setw(10) << left << stud.vardas << "|"
+             << setw(15) << right << stud.pavarde << "|"
+             << fixed << std::setprecision(2) << setw(20) << right << stud.gal_rezultatas << "|"
+             << fixed << std::setprecision(2) << setw(20) << right << stud.mediana << endl;
+    }
+}
